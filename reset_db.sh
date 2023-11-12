ef8a6f368233d7b41ee93e0645e1eee777e1c5ba
Derrick Liu <derrick56007@hotmail.com>
2023-11-12 15:17:27 -0800

    reset db script

---
+++ reset_db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker cp ./reset.sql db:/
+docker exec db /bin/sh -c 'psql -h localhost -d postgres -U postgres -p 5432 -a -q -f /reset.sql'
+
+# -h PostgreSQL server IP address
+# -d database name
+# -U user name
+# -p port which PostgreSQL server is listening on
+# -f path to SQL script
+# -a all echo
+# -q quiet
