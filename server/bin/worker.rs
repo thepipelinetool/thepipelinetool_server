e821cb8f7321da042ff821af6ebf2061e7f4e5d7
thepipelinetool <derrick56007@icloud.com>
2023-12-16 13:35:31 -0800

    remove unnecessary clone

--- server/bin/worker.rs
+++ server/bin/worker.rs
@@ -13,13 +13,14 @@ async fn main() {
 
     loop {
         if let Some(ordered_queued_task) = dummy.pop_priority_queue() {
-            let dag_name = &ordered_queued_task.queued_task.dag_name.clone();
-
-            let mut runner = RedisRunner::from_local_dag(dag_name, pool.clone());
+            let mut runner = RedisRunner::from_local_dag(
+                &ordered_queued_task.queued_task.dag_name,
+                pool.clone(),
+            );
             runner.work(
                 ordered_queued_task.queued_task.run_id,
                 &ordered_queued_task,
-                _get_dag_path_by_name(dag_name),
+                _get_dag_path_by_name(&ordered_queued_task.queued_task.dag_name),
             );
             runner.remove_from_temp_queue(&ordered_queued_task.queued_task);
         } else {
